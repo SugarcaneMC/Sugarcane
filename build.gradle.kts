78ae9c2188ae3c0ca30a1991d33930daf32121cf
Toffikk <slodkitofik@gmail.com>
2021-08-19 16:04:05 +0200

    Paperweight 1.1.11

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     java
     `maven-publish`
     id("com.github.johnrengelman.shadow") version "7.0.0" apply false
-    id("io.papermc.paperweight.patcher") version "1.1.9"
+    id("io.papermc.paperweight.patcher") version "1.1.11"
 }
 
 repositories {
